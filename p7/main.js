b873e8193b3cb57d95c125774d666c1eb86be98b
David Avila <davidavila6002@gmail.com>
2023-03-15 18:45:31 -0600

    p5 , p6 , p7

---
+++ p7/main.js
@@ -0,0 +1,25 @@
+// let a = true;
+
+// let b = false;
+
+let numero1 = 7;
+
+let numero2 = 7;
+
+let valor = numero1 === numero2; // true
+
+//************************************************ */
+
+// Funcion que regresa un boleano.
+function numeritos() {
+  let simbolo = 0;
+
+  return simbolo === 0;
+}
+
+// Condicional
+
+if (!numeritos()) {
+  // Se ejecuta el cuerpo siempre que sea true
+  console.log("Holaaa");
+}
